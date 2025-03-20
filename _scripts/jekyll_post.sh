1831a818e31d80b4c512ff61192c9984bb84fcfd
Rakou <julien.legall@gmail.com>
2025-03-20 21:47:10 +0100

    update gemfile du to Ubuntu 24.04 update

--- _scripts/jekyll_post.sh
+++ _scripts/jekyll_post.sh
@@ -6,4 +6,4 @@ year=`date +'%Y'`
 post_path=`jekyll-post -d ./_posts/$year/ "$1" -a 'layout:"post"' 'image:"images/2022/wallhaven-42v2xx.png"' 'categories: ["article"]'`
 
 # add arg for make conditionnal this intruction
-vi $post_path;
+# vi $post_path;
